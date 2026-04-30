4bf73fc6eae530482ad6157d810db8e26ba3e93d
flik2002 <5003698@qq.com>
2026-04-30 17:08:17 +0800

    Cleanup - remove unnecessary files

--- openclaw-monitor/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting OpenClaw Monitor API Wrapper...
-cd /d "%~dp0"
-start /B node server.js
-echo Service started on port 3000
-echo Press any key to exit this window (service will keep running)
-pause >nul
